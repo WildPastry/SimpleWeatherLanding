0850194cd4611f0f234a1e73dd55c8d2b40d0262
WildPastry <mykdsn@gmail.com>
2021-06-20 12:13:11 +1200

    Added redux starter

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import {Container} from 'react-bootstrap';
 import Header from './sections/Header';
 import Main from './sections/Main';
 import Footer from './sections/Footer';
+
+// import { NewNoteInput } from "./sections/NewNoteInput";
+// import { useSelector } from "react-redux";
+// import { NotesState } from "./redux/reducers/notesReducer";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/main.scss';
 
 const App = () => {
+
+  // const notes = useSelector<NotesState, NotesState["notes"]>(
+  //   (state) => state.notes
+  // );
+
   return (
     <Container fluid className='my-3'>
       <Header />
--- src/sections/Main.tsx
+++ src/sections/Main.tsx
@@ -1,11 +1,36 @@
 import logo from '.././logo.svg';
 import {Col, Row} from 'react-bootstrap';
-
+import NewNoteInput from "./NewNoteInput";
+import { useAppDispatch } from '../redux/hooks';
+import { useSelector } from "react-redux";
+import { NotesState } from "../redux/reducers/notesReducer";
+import { addNote } from "../redux/actions/loadAction";
 const Main = () => {
+
+  const notes = useSelector<NotesState, NotesState["notes"]>(
+    (state) => state.notes
+  );
+  
+  const dispatch = useAppDispatch();
+
+  const onAddNote = (note: string) => {
+    dispatch(addNote(note));
+  };
+
   return (
     <Row>
       <Col xs={12}><p>MAIN</p></Col>
       <Col xs={12}><img src={logo} className='appLogo' alt='SimpleWeather' /></Col>
+      <Col xs={6}>
+      <NewNoteInput addNote={onAddNote} />
+        </Col>
+         <Col xs={6}>
+        <ul>
+                  {notes.map((note) => {
+          return <li key={note}>{note}</li>;
+        })}
+        </ul>
+        </Col>
     </Row>
   );
 };
