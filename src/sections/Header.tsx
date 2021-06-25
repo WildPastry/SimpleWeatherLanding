b0079f8201ae6264999f160702dd85580dd76569
WildPastry <mykdsn@gmail.com>
2021-06-25 17:18:56 +1200

    Added heading styles

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,14 @@
-import {Col, Row} from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 
 const Header = () => {
-  return (
-    <Row className='my-3'>
-      <Col xs={12}><span className='fontLt me-1'>SIMPLE</span><span className='fontBd'>WEATHER</span></Col>
-    </Row>
-  );
+	return (
+		<Row className='my-3'>
+			<Col xs={12}>
+				<span className='headingLt me-1'>SIMPLE</span>
+				<span className='headingBd'>WEATHER</span>
+			</Col>
+		</Row>
+	);
 };
 
 Header.displayName = 'Header';
