68392f254bd185e759e41cf205ec0ad30ea65561
WildPastry <mykdsn@gmail.com>
2021-07-10 21:14:47 +1200

    Save point

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -12,13 +12,13 @@ const Intro: React.FC = () => {
 	return (
 		<Container fluid='lg' className='my-3'>
 			<Row className='my-3'>
-				<Col xs={10}>
+				<Col xs={12} sm={10} md={9}>
 					<h1 className='h1Bd'>Does what the name suggests.</h1>
 					<h2 className='h2Rg'>
 						Fast, accurate, and intuitive weather app designed for iOS and Android.
 					</h2>
 				</Col>
-				<Col xs={2} className='d-flex justify-content-center align-items-center'>
+				<Col xs={12} sm={2} md={3} className='d-flex justify-content-center align-items-center bg-dark'>
 					<Icon />
 				</Col>
 			</Row>
