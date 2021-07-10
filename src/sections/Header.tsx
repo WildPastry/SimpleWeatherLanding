2a172e27ecb3c51fac03dcb57e0e812ebe8556be
WildPastry <mykdsn@gmail.com>
2021-07-10 15:50:59 +1200

    Adding mobile styles

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,11 +4,11 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 const Header = () => {
 	return (
 		<Container className='my-2' fluid='lg'>
-			<Navbar collapseOnSelect expand='lg' bg='light' variant='light'>
-				<Navbar.Brand className='p-0 d-flex align-items-center'>
-						<img src={logo} className='appLogoNav' alt='SimpleWeather' />
-						<span className='h2Lt me-1'>SIMPLE</span>
-						<span className='h2Bd spotBlue'>WEATHER</span>
+			<Navbar className='navbar-xxs' collapseOnSelect expand='md' bg='light' variant='light'>
+				<Navbar.Brand className='p-0'>
+					<img src={logo} className='appLogoNav' alt='SimpleWeather' />
+					<span className='h2Lt me-1'>SIMPLE</span>
+					<span className='h2Bd spotBlue'>WEATHER</span>
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 				<Navbar.Collapse id='responsive-navbar-nav' className='justify-content-end'>
@@ -17,12 +17,7 @@ const Header = () => {
 						<Nav.Link href='#Locations'>Locations</Nav.Link>
 						<Nav.Link href='#Weather'>Weather</Nav.Link>
 						<Nav.Link href='#Features'>Features</Nav.Link>
-						<Nav.Link
-							href='https://mikeparker.co.nz/'
-							target='_blank'
-							rel='noopener noreferrer'>
-							Developer
-						</Nav.Link>
+						<Nav.Link href='#About'>About</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Navbar>
--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -6,17 +6,15 @@ const Intro = () => {
 		<Container fluid='lg' className='my-3'>
 			<Row className='my-3'>
 				<Col xs={12}>
-					<h1 className='h1Bd'>
-						Does what it's name suggests. 
-					</h1>
+					<h1 className='h1Bd'>Does what it's name suggests.</h1>
 				</Col>
 				<Col xs={12}>
-					<h2 className='h2Rg'>Fast, accurate, and intuitive weather app designed for iOS and Android.</h2>
-				</Col>
-				<Col xs={12}>
-					<img src={googleBadge} className='googleBadge' alt='Google Play Store' />
+					<h2 className='h2Rg'>
+						Fast, accurate, and intuitive weather app designed for iOS and Android.
+					</h2>
 				</Col>
 			</Row>
+			<img src={googleBadge} className='googleBadge' alt='Google Play Store' />
 		</Container>
 	);
 };
