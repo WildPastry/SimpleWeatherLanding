4de865b4de5995d56ec908a63cc6b9c649847108
WildPastry <mykdsn@gmail.com>
2021-07-17 21:29:56 +1200

    Added scroll to top icon

--- src/components/TopIcon.tsx
+++ src/components/TopIcon.tsx
@@ -1,3 +1,9 @@
+// import frameworks
+import { useState } from 'react';
+
+// import icon
+import { ArrowUpOutline } from 'react-ionicons';
+
 // scroll function from APP
 interface ScrollProps {
 	scrollToPane(num: number): any;
@@ -5,7 +11,29 @@ interface ScrollProps {
 
 // TOPICON
 const TopIcon: React.FC<ScrollProps> = (props) => {
-	return <div className='topIcon' onClick={() => props.scrollToPane(0)}>TOP</div>;
+	// scroll icon display logic
+  const [showScroll, setShowScroll] = useState(false)
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400){
+      setShowScroll(true)
+    } else if (showScroll && window.pageYOffset <= 400){
+      setShowScroll(false)
+    }
+  };
+
+	window.addEventListener('scroll', checkScrollTop)
+	return (
+		<ArrowUpOutline
+		  style={{display: showScroll ? 'block' : 'none'}}
+			onClick={() => props.scrollToPane(0)}
+			cssClasses='topIcon'
+			color={'#1faadb'}
+			title={'TOP'}
+			height='65px'
+			width='65px'
+		/>
+	);
 };
 
 // EXPORT TOPICON
