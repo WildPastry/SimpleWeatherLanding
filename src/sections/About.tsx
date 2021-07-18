9f2e491e9995dc72a89161ddab9504c1a8cebf5d
WildPastry <mykdsn@gmail.com>
2021-07-19 07:08:39 +1200

    Adjusted About Screen

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -6,7 +6,7 @@ const About: React.FC = () => {
 	return (
 		<Container fluid='lg' className='my-3'>
 			<Row className='my-3'>
-				<Col xs={12} sm={6} className='fontRg'>
+				<Col xs={12} className='fontRg'>
 					<h2 className='h2Bd'>About.</h2>
 						<p>Quisque et nunc euismod, porttitor urna sed, facilisis arcu</p>
 						<p>Cras vel diam facilisis justo accumsan efficitur quis eu risus</p>
@@ -14,6 +14,7 @@ const About: React.FC = () => {
 						<p>Fusce consectetur felis maximus, lacinia dolor at, vehicula massa</p>
 						<p>Phasellus ac tellus tristique orci molestie auctor</p>
 						<p>Aliquam efficitur ante maximus magna aliquet, eget vestibulum leo tempor</p>
+						<h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo justo ut libero pretium, sit amet lobortis turpis auctor. Morbi vel libero purus. Vestibulum dapibus felis id lectus fermentum condimentum. Aliquam dictum ut diam tempus porttitor. Morbi egestas elit a odio lobortis aliquet. Nullam sollicitudin, odio et tempor cursus, eros felis hendrerit tortor, a egestas nibh ligula quis augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget felis ut libero congue accumsan sed sed eros. </h5>
 				</Col>
 			</Row>
 		</Container>
