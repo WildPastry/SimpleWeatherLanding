d30aa7b7283ebc4e2745543c3d49da47392c26df
WildPastry <mykdsn@gmail.com>
2021-08-02 20:40:09 +1200

    Added new image, 50% through privacy page

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -4,13 +4,15 @@ import { Col, Container, Row } from 'react-bootstrap';
 // About
 const About: React.FC = () => {
 	return (
-		<Container fluid='lg' className='sectionContainer'>
-			<Row>
-				<Col sm={12}>
-					<section className='aboutText simpleWeather'>A SIMPLE APPROACH</section>
-				</Col>
-			</Row>
-		</Container>
+		<section className='positionRelative weatherContainer bgimg-4'>
+			<Container fluid='lg'>
+				<Row>
+					<Col sm={12}>
+						<section className='aboutText light'>A SIMPLE APPROACH</section>
+					</Col>
+				</Row>
+			</Container>
+		</section>
 	);
 };
 
